95435d659288a3249fe51b80e071e7994b9aa88b
sktgithubl <skt.sipun123@gmail.com>
2021-12-05 12:49:37 +0530

    enhancing the hello world service to return a bean

--- src/main/java/com/skt/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/skt/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 	
-	//GET
-	//URI- /hello-world
-	// method - "Hello World"
 	//@RequestMapping(method=RequestMethod.GET, path="/hello-world")
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	//hello-world-bean
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
 }
